3ab217a9ff7f56f522d67ef5b4447c3788527a03
Pawel93code <77625538+Pawel93code@users.noreply.github.com>
2021-03-02 15:11:04 +0000

    easystart

---
+++ zmiana nazwista.py
@@ -0,0 +1,13 @@
+firstName = 'Kasia'
+familyName = 'Sowa'
+lastName = 'Mrugala'
+newName = firstName + " " + familyName + " " + lastName
+print(newName)
+
+music = r'"Unicerlas Fanfare" Jerry Goldsmith "Happy Together" Garry Bonner "I\'m a Man" Steve Winwood'
+print(music)
+music = '"Unicerlas Fanfare" Jerry Goldsmith\n"Happy Together" Garry Bonner\n"I\'m a Man" Steve Winwood'
+print(music)
+print("(\\(\\")
+print("(-.-)")
+print('0_(")(")')
